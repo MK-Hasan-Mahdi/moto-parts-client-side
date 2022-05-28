4bbaca628952d9419d44ce5cc516032b6a16042c
mk-hasan <mkhasan6891@gmail.com>
2022-05-28 23:35:45 +0600

    some design related recheked

--- src/Pages/Dashboard/User.js
+++ src/Pages/Dashboard/User.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
 const User = ({ user, refetch }) => {
     const { email, role } = user;
+    const [users, setUsers] = useState([]);
+
+
+    useEffect(() => {
+        const url = `https://peaceful-dusk-44249.herokuapp.com/user`;
+        // console.log(url);
+        fetch(url, {
+            method: "GET",
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => setUsers(data));
+    }, []);
 
     const alreadyAdmin = () => {
         toast.error('I am already Admin')
@@ -30,12 +46,30 @@ const User = ({ user, refetch }) => {
             })
     }
 
+    const handleDeleteUser = (id) => {
+        const proceedDelete = window.confirm('Confirm delete');
+        if (proceedDelete) {
+            const url = `https://peaceful-dusk-44249.herokuapp.com/user/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    // console.log(data);
+                    const remainingUser = users.filter(user => user._id !== id)
+                    setUsers(remainingUser);
+                    refetch();
+                    toast.success(`User Deleted`)
+                })
+        }
+    }
+
     return (
         <tr className='hover'>
             <th>{email}</th>
             <td></td>
             <td>{role !== 'admin' ? <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button> : <butoon onClick={alreadyAdmin} className='btn btn-xs  btn-success'>Admin</butoon>}</td>
-            <td><button class="btn btn-xs">Remove User</button></td>
+            <td><button onClick={() => handleDeleteUser(user._id)} class="btn btn-xs">Remove User</button></td>
         </tr>
     );
 };
